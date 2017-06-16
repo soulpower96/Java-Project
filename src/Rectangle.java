5a566167b96daa1928252a1b2555ab37bd6534ef
Suresh Manandhar <suresh@soulpower966.mylabserver.com>
2017-06-16 16:00:05 +0000

    fixing codes and add junit in lib

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,18 +1,17 @@
 public class Rectangle {
-	public int length;
-	public int width;
+  public int length;
+  public int width;
 
-	public Rectangle(int length, int width) {
-		this.length = length;
-		this.width = width;
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  public int getArea() {
+    return length * width;
+  }
+
+  public int getPerimeter() {
+    return 2 * (length + width);
+  }
 }
-
-	public int getArea() {
-		return length * width;
-	}
-
-public int getPerimeter() {
-	return 2 * (length + width);
-	}
-}
-
